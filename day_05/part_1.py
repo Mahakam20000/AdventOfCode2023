e73c59c5b3704c4c198eee3da344ca2f7da74a0d
Emiliano <emimart@gmail.com>
2023-12-10 18:41:59 +0100

    Day 05, part2_v2 doesn't work with the real input :(

---
+++ day_05/part_1.py
@@ -0,0 +1,140 @@
+"""--- Day 5: If You Give A Seed A Fertilizer ---
+You take the boat and find the gardener right where you were told he would be: managing a giant "garden" that looks more to you like a farm.
+
+"A water source? Island Island is the water source!" You point out that Snow Island isn't receiving any water.
+
+"Oh, we had to stop the water because we ran out of sand to filter it with! Can't make snow with dirty water. Don't worry, I'm sure we'll get more sand soon; we only turned off the water a few days... weeks... oh no." His face sinks into a look of horrified realization.
+
+"I've been so busy making sure everyone here has food that I completely forgot to check why we stopped getting more sand! There's a ferry leaving soon that is headed over in that direction - it's much faster than your boat. Could you please go check it out?"
+
+You barely have time to agree to this request when he brings up another. "While you wait for the ferry, maybe you can help us with our food production problem. The latest Island Island Almanac just arrived and we're having trouble making sense of it."
+
+The almanac (your puzzle input) lists all of the seeds that need to be planted. It also lists what type of soil to use with each kind of seed, what type of fertilizer to use with each kind of soil, what type of water to use with each kind of fertilizer, and so on. Every type of seed, soil, fertilizer and so on is identified with a number, but numbers are reused by each category - that is, soil 123 and fertilizer 123 aren't necessarily related to each other.
+
+For example:
+
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+The almanac starts by listing which seeds need to be planted: seeds 79, 14, 55, and 13.
+
+The rest of the almanac contains a list of maps which describe how to convert numbers from a source category into numbers in a destination category. That is, the section that starts with seed-to-soil map: describes how to convert a seed number (the source) to a soil number (the destination). This lets the gardener and his team know which soil to use with which seeds, which water to use with which fertilizer, and so on.
+
+Rather than list every source number and its corresponding destination number one by one, the maps describe entire ranges of numbers that can be converted. Each line within a map contains three numbers: the destination range start, the source range start, and the range length.
+
+Consider again the example seed-to-soil map:
+
+50 98 2
+52 50 48
+The first line has a destination range start of 50, a source range start of 98, and a range length of 2. This line means that the source range starts at 98 and contains two values: 98 and 99. The destination range is the same length, but it starts at 50, so its two values are 50 and 51. With this information, you know that seed number 98 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
+
+The second line means that the source range starts at 50 and contains 48 values: 50, 51, ..., 96, 97. This corresponds to a destination range starting at 52 and also containing 48 values: 52, 53, ..., 98, 99. So, seed number 53 corresponds to soil number 55.
+
+Any source numbers that aren't mapped correspond to the same destination number. So, seed number 10 corresponds to soil number 10.
+
+So, the entire list of seed numbers and their corresponding soil numbers looks like this:
+
+seed  soil
+0     0
+1     1
+...   ...
+48    48
+49    49
+50    52
+51    53
+...   ...
+96    98
+97    99
+98    50
+99    51
+With this map, you can look up the soil number required for each initial seed number:
+
+Seed number 79 corresponds to soil number 81.
+Seed number 14 corresponds to soil number 14.
+Seed number 55 corresponds to soil number 57.
+Seed number 13 corresponds to soil number 13.
+The gardener and his team want to get started as soon as possible, so they'd like to know the closest location that needs a seed. Using these maps, find the lowest location number that corresponds to any of the initial seeds. To do this, you'll need to convert each seed number through other categories until you can find its corresponding location number. In this example, the corresponding types are:
+
+Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+So, the lowest location number in this example is 35.
+
+What is the lowest location number that corresponds to any of the initial seed numbers?"""
+
+
+def load_input(input: list[str]) -> None:
+    for seed in input[0].split(':')[1].split():
+        seeds.append(int(seed))
+    dict_num: int = -1
+    for line_num in range(1, len(input)):
+        if input[line_num][0].isdigit():
+            tmp: list[int] = []
+            for number in input[line_num].split():
+                tmp.append(int(number))
+            transformations[dict_num][f"{tmp[1]}-{tmp[1]+tmp[2]-1}"] = tmp[0]-tmp[1]
+        elif input[line_num][0].isalpha():
+            transformations.append({})
+            dict_num += 1
+
+
+
+def apply_transformations(seeds: list[int], transformations: list[dict[str, int]]) -> list[int]:
+    seeds_aux: list[int] = seeds.copy()
+    for i in range(len(seeds_aux)):
+        for transformation in transformations:
+            for source_range in [ x for x in transformation.keys() ]:
+                if seeds_aux[i] in range(int(source_range.split('-')[0]), int(source_range.split('-')[1])+1):
+                    seeds_aux[i] += transformation.get(source_range)
+                    break
+    return seeds_aux
+
+
+
+
+
+#with open('day_05/test_input_1', 'r') as input:
+with open('day_05/input', 'r') as input:
+    l = input.readlines()
+seeds: list[int] = []
+#matrices: list[list[list[int]]] = []
+transformations: list[dict[str, int]] = []
+load_input(l)
+#print(seeds)
+#print(transformations)
+res = apply_transformations(seeds, transformations)
+print(min(res))
+
+
+
---
+++ day_05/part_2_v2.py
@@ -0,0 +1,214 @@
+"""--- Part Two ---
+Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?"""
+
+
+import heapq
+
+
+def load_input(input: list[str]) -> None:
+    """
+        Input:  a list of strings representing the lines of the input file
+
+        Output: side effect on the lists created before.
+                For seeds a list of lists of int will be used. i.e.:
+                [[start, end], [start, end], [start, end] ...]
+                For transformations a list of lists of tuples containing a 
+                tuple of two int and an int will be used.
+                maps[map[transformation((start, end), add)]]
+                maps is the list that contains all the map;
+                map are the lists that contain the transformations;
+                transformation is a tuple composed of:
+                    -   a tuple that indicates the interval where the 
+                        transformation is applied
+                    -   an int that indicates what number must be added (it can 
+                        be negative, too) to apply the transformation at the 
+                        values in the interval given by the tuple
+    """
+    s: list[int] = [ int(x) for x in input[0].split(':')[1].split() ]
+    for i in range(0, len(s), 2):
+        interval: list[int] = []
+        interval.extend([s[i], s[i]+s[i+1]-1])
+        heapq.heappush(seeds, interval)
+    list_num: int = -1
+    for line_num in range(1, len(input)):
+        if input[line_num][0].isdigit():
+            tmp_transformations_list: list[int] = []
+            for number in input[line_num].split():
+                tmp_transformations_list.append(int(number))
+            aux_tuple: tuple[int, int] = (tmp_transformations_list[1], tmp_transformations_list[1]+tmp_transformations_list[2]-1)
+            heapq.heappush(transformations[list_num], (aux_tuple, tmp_transformations_list[0]-tmp_transformations_list[1]))
+        elif input[line_num][0].isalpha():
+            transformations.append([])
+            list_num += 1
+
+
+
+def intersection(interval: list[int], transformation: tuple[int, int]) -> list[list[int]]:
+    """
+        Input:  interval:           a list of two integers representing an 
+                                    interval fo values
+                transformations:    a tuple of two integers representing an 
+                                    interval of values
+        
+        Output: a list of tree lists containing two integers each.
+                The first list represents the value of interval that are before 
+                the values of transformation, so these values must be not 
+                transformed.
+                The second list represents the interval of value of interval 
+                that are in the interval of transformation, so these values 
+                must be transformed.
+                The third list represents the interval of value of interval 
+                that are after the values of transformations, so these values 
+                must not be transformed.
+    """
+    if interval[1] < transformation[0]:
+        return [interval, [], []]
+    if interval[0] > transformation[1]:
+        return [[], [], interval]
+    res: list[list[int]] = [[], [], []]
+    if interval[0] < transformation[0]:
+        res[0] = [interval[0], transformation[0]-1]
+        res[1].append(transformation[1])
+    else:
+        res[1].append(interval[0])
+    if interval[1] > transformation[1]:
+        res[2] = [transformation[1]+1, interval[1]]
+        res[1].append(transformation[1])
+    else:
+        res[1].append(interval[1])
+    return res
+
+
+
+def interval_binary_search(interval: list[int], transformations_list: list[tuple[int, int]], lim_sx: int = 0, lim_dx: int = -1) -> int:
+    """
+        Input:  interval:               a list of two integers representing an 
+                                        interval of values
+                transformations_list:   a list of tuple of two integers, each 
+                                        tuple represents an interval of value
+                lim_sx:                 an integer representing the limit sx of 
+                                        the transformations_list during the 
+                                        binary search.
+                lim_dx                  an integer representing the limit dx of 
+                                        the transformations_list during the 
+                                        binary search.
+        
+        Output: an integer representing index of transformations_list that 
+                indicate the first interval of values that intersect the given 
+                interval.
+                -1 if no intervals in transformation_list intersect the given 
+                interval.
+    """
+    if lim_dx == -1:
+        lim_dx = len(transformations_list)-1
+    if lim_dx == lim_sx:
+        if intersection(interval, transformations_list[lim_sx])[1] == []:
+            return -1
+        return lim_sx
+    index = int((lim_sx + lim_dx)/2)
+    if transformations_list[index][1] < interval[0]:
+        return interval_binary_search(interval, transformations_list, index+1, lim_dx)
+    if transformations_list[index][0] > interval[1]:
+        return interval_binary_search(interval, transformations_list, lim_sx, index)
+    while index > 0:
+        if intersection(interval, transformations_list[index-1])[1] != []:
+            index =- 1
+        else:
+            break
+    return index
+
+
+
+def apply_transformation(interval: list[int], transformation: int) -> list[int]:
+    return [interval[0]+transformation, interval[1]+transformation]
+
+
+
+def merge_intervals(seeds: list[list[int]]) -> list[list[int]]:
+    """
+        Input:  a sorted list of intervals in the [start, end] form
+
+        Output: a sorted list of intervals where the overlapping intervals of 
+                the input list are merged
+    """
+    res: list[list[int]] = []
+    while len(seeds) > 1:
+        e1 = heapq.heappop(seeds)
+        e2 = heapq.heappop(seeds)
+        if e1[1] > e2[0]:
+            heapq.heappush(seeds, [e1[0], max(e1[1], e2[1])])
+            continue
+        heapq.heappush(res, e1)
+        heapq.heappush(seeds, e2)
+    if len(seeds) == 1:
+        aux = heapq.heappop(seeds)
+        heapq.heappush(res, aux)
+    return res
+
+
+
+
+
+def elaborate(seeds: [list[list[int, int]]], transformations_lists: list[list[tuple[tuple[int, int], int]]], num_trans_list: int = 0) -> list[list[int]]:
+    """
+        Input:  seeds:                  list of intervals of values to transform
+                transformations_lists:  lists of list of transformations. Lists 
+                                        of transformations must be applied to 
+                                        elements in seeds, in order. A 
+                                        trasnformation is a 
+                                        tuple[tuple[int, int], int] where the 
+                                        first tuple says the range where the 
+                                        transformation must be applied and the 
+                                        last integer what number must be add to 
+                                        do the transformation. The last number 
+                                        can be positive or negative.
+                num_trans_list: int     a number that says what list of 
+                                        transformations to use.
+        
+        Output: a list of lists of two integers. Every list indicate a range of 
+                values representing an interval
+    """
+    if num_trans_list == len(transformations_lists):
+        return seeds
+    elaborated_intervals: list[list[int]] = []
+    trans_list: list[tuple[tuple[int, int], int]] = [ heapq.heappop(transformations_lists[num_trans_list]) for _ in range(len(transformations_lists[num_trans_list])) ]
+    while len(seeds) > 0:
+        interval = heapq.heappop(seeds)
+        first_index: int = interval_binary_search(interval, [ x[0] for x in trans_list ])
+        if first_index == -1:
+            heapq.heappush(elaborated_intervals, interval)
+            continue
+        aux: list[list[int]] = intersection(interval, trans_list[first_index][0])
+        if aux[0] != []:
+            heapq.heappush(elaborated_intervals, aux[0])
+        heapq.heappush(elaborated_intervals, apply_transformation(interval, trans_list[first_index][1]))
+        if aux[2] != []:
+            heapq.heappush(seeds, aux[2])
+    return elaborate(merge_intervals(elaborated_intervals), transformations_lists, num_trans_list+1)
+
+
+
+
+
+
+
+with open('day_05/input', 'r') as input:
+#with open('day_05/test_input_1', 'r') as input:
+    l = input.readlines()
+seeds: list[list[int]] = []
+transformations: list[list[tuple[tuple[int, int], int]]] = []
+load_input(l)
+res = elaborate(seeds, transformations)
+print(f"res: {res}")
+print(min(min(res)))
